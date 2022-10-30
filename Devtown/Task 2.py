7b6a359021c27cc819a9c37f804093e632a9a46b
Joel Joby <joeljoby369@gmail.com>
2022-10-31 04:50:04 +0530

    Tasks complete

--- Devtown/Task 2.py
+++ Devtown/Task 2.py
@@ -1,3 +1,16 @@
-elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
-            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+#Try your hand at working with nested dictionaries. Add another entry, 'is_noble_gas,' to each dictionary in the elements dictionary. After inserting the new entries you should be able to perform these lookups:
+
+#print(elements['hydrogen']['is_noble_gas'])
+#False
+
+#print(elements['helium']['is_noble_gas'])
+#True
+
+
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H', 'Is_Noble': "False"},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He', "Is_Noble": "True"}}
+
+print(elements['helium']["Is_Noble"])
+print(elements['hydrogen']['Is_Noble'])
+
 
--- Devtown/Task populations.py
+++ Devtown/Task populations.py
@@ -1,2 +1,14 @@
+#Define a dictionary named population that contains this data:
+
+#Keys -> Values
+
+#New York -> 17.8
+
+#Spain -> 13.3
+
+#Dhaka -> 13.0
+
+#Mumbai -> 12.5
+
 populations = {"ney york":17.8, "spain":13.3, "Dhaka":13.0, "Mumbai":12.5}
 print(populations)
